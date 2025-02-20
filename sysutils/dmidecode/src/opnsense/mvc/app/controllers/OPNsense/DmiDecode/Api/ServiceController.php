12e5780b725ba93a17f45861f539a9ee2e9d32a0
Ethazeriel <ethazeriel@gmail.com>
2025-02-20 14:10:03 +1300

    simplify php

--- sysutils/dmidecode/src/opnsense/mvc/app/controllers/OPNsense/DmiDecode/Api/ServiceController.php
+++ sysutils/dmidecode/src/opnsense/mvc/app/controllers/OPNsense/DmiDecode/Api/ServiceController.php
@@ -11,14 +11,8 @@ class ServiceController extends ApiControllerBase
     $system = array();
     $bios = array();
     if ($this->request->isGet()) {
-      foreach (explode("\n", trim((new Backend())->configdRun('dmidecode system'))) as $item) {
-        [$newkey, $newval] = explode(" = ", $item);
-        $system[$newkey] = $newval;
-      }
-      foreach (explode("\n", trim((new Backend())->configdRun('dmidecode bios'))) as $item) {
-        [$newkey, $newval] = explode(" = ", $item);
-        $bios[$newkey] = $newval;
-      }
+      $system = parse_ini_string(trim((new Backend())->configdRun('dmidecode system')), FALSE, INI_SCANNER_RAW);
+      $bios = parse_ini_string(trim((new Backend())->configdRun('dmidecode bios')), FALSE, INI_SCANNER_RAW);
       $status = "ok";
     }
     return ["status" => $status, "system" => $system, "bios" => $bios];
