98961e9006e03de6278d409bdb81dbe373a9fe23
Ethazeriel <ethazeriel@gmail.com>
2025-02-20 13:15:31 +1300

    translations, headers, cleanup

--- sysutils/dmidecode/src/www/js/widgets/DMIDecode.js
+++ sysutils/dmidecode/src/www/js/widgets/DMIDecode.js
@@ -1,3 +1,31 @@
+/*
+ * Copyright (C) 2025 Neil Merchant
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import BaseTableWidget from 'widget-base-table';
+
 export default class DMIDecode extends BaseTableWidget {
   constructor() {
     super();
@@ -7,22 +35,21 @@ export default class DMIDecode extends BaseTableWidget {
 
   getGridOptions() {
     return {
-      sizeToContent: 350
+      sizeToContent: 650
     
     }
   }
 
   getMarkup() {
     let $container = $('<div></div>');
-    let $dmi_table = super.createTable('dmiTable', {
-      headerPosition: 'top',
-      headers: [
-        'item',
-        'value'
-      ]
-    });
-
-    $container.append($dmi_table);
+    // make table for system output, add header
+    let $system_table = super.createTable('system-table', { headerPosition: 'none' });
+    $container.append(`<h3>${this.translations.system}</h3>`)
+    $container.append($system_table);
+    // same for bios output
+    let $bios_table = super.createTable('bios-table', { headerPosition: 'none' });
+    $container.append(`<h3>${this.translations.bios}</h3>`)
+    $container.append($bios_table);
     return $container;
   }
 
@@ -36,19 +63,28 @@ export default class DMIDecode extends BaseTableWidget {
   }
 
   processDMIData(data) {
-    const rows = [];
+    const sysrows = [];
     for (const [key, value] of Object.entries(data.system)) {
-      // do something with it, I dunno
       const row = [];
-      row.push(`<div>${key}</div>`, `<div>${value}</div>`);
-      rows.push(row);
+      // try to find translation for key, fallback to output value
+      // have to split on spaces here because those aren't valid in xml tags
+      const translationIndex = key.split(" ")[0]
+      const dispKey = this.translations[translationIndex] || key
+      row.push(`<div><b>${dispKey}</b></div>`, `<div>${value}</div>`);
+      sysrows.push(row);
     }
+    const biosrows = [];
     for (const [key, value] of Object.entries(data.bios)) {
       const row = [];
-      row.push(`<div>${key}</div>`, `<div>${value}</div>`);
-      rows.push(row);
+      // try to find translation for key, fallback to output value
+      // have to split on spaces here because those aren't valid in xml tags
+      const translationIndex = key.split(" ")[0]
+      const dispKey = this.translations[translationIndex] || key
+      row.push(`<div><b>${dispKey}</b></div>`, `<div>${value}</div>`);
+      biosrows.push(row);
     }
-    super.updateTable('dmiTable', rows);
+    super.updateTable('system-table', sysrows);
+    super.updateTable('bios-table', biosrows);
   }
 
   displayError(message) {
@@ -61,31 +97,3 @@ export default class DMIDecode extends BaseTableWidget {
 }
 
 }
-
-
-
-
-
-
-// $hardwareData = parse_ini_string(configd_run("dmidecode system"), FALSE, INI_SCANNER_RAW);
-// $biosData = parse_ini_string(configd_run("dmidecode bios"), FALSE, INI_SCANNER_RAW);
-
-// ?>
-// <table class="table table-striped table-condensed">
-//     <tbody>
-//         <tr><th colspan="2"><?=gettext("Platform");?></th></tr>
-//         <?php foreach($hardwareData as $key => $val) { ?>
-//         <tr>
-//             <td style="width: 30%;"><?=gettext($key);?></td>
-//             <td><?=html_safe($val);?></td>
-//         </tr>
-//         <?php } ?>
-//         <tr><th colspan="2"><?=gettext("BIOS");?></th></tr>
-//         <?php foreach($biosData as $key => $val) { ?>
-//         <tr>
-//             <td style="width: 30%;"><?=gettext($key);?></td>
-//             <td><?=html_safe($val);?></td>
-//         </tr>
-//         <?php } ?>
-//     </tbody>
-// </table>
